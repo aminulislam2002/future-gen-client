7087e5981d178efdb5fd4b8e990ecb68fc3cf4cb
aminulislam2002 <aminulislamemon2002@gmail.com>
2024-02-27 21:18:25 +0600

    design basic navbar

--- src/Pages/HomePage/SharedPages/NavBar/NavBar.jsx
+++ src/Pages/HomePage/SharedPages/NavBar/NavBar.jsx
@@ -1,10 +1,47 @@
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
+import Form from "react-bootstrap/Form";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+
+import "./NavBar.css";
 
 const NavBar = () => {
-    return (
-        <div>
-            <h1>NavBar</h1>
-        </div>
-    );
+  return (
+    <Navbar
+      expand="lg"
+      fixed="top"
+      style={{ backgroundImage: `linear-gradient(90deg, #111c58 0, #5c2782 51%, #111c58)`, color: "white" }}
+    >
+      <Container fluid className="container mx-auto">
+        <Navbar.Brand className="text-white" href="/">
+          FutureGen IT Media
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbarScroll" />
+        <Navbar.Collapse id="navbarScroll">
+          <Nav className="ms-auto me-2 my-2 my-lg-0" style={{ maxHeight: "100px" }} navbarScroll>
+            <Nav.Link className="text-white" href="/">
+              Home
+            </Nav.Link>
+            <Nav.Link className="text-white" href="/about-us">
+              About Us
+            </Nav.Link>
+            <Nav.Link className="text-white" href="/services">
+              Services
+            </Nav.Link>
+            <Nav.Link className="text-white" href="/blog">
+              Blog
+            </Nav.Link>
+          </Nav>
+          <Form className="d-flex">
+            <Button variant="outline-success" className="gradient border-0 text-white">
+              Say Hi
+            </Button>
+          </Form>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 };
 
 export default NavBar;
